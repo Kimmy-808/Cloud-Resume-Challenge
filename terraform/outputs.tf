4700dd92348deb6e25cc9d1ecef79b4c2e8e8e96
Kimmy-808 <kcruz18@valenciacollege.edu>
2026-01-04 21:51:33 -0500

    Output counter API URL

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,11 @@
 output "region" {
   value = var.aws_region
 }
+
+output "counter_api_base_url" {
+  value = aws_apigatewayv2_api.counter_api.api_endpoint
+}
+
+output "counter_api_url" {
+  value = "${aws_apigatewayv2_api.counter_api.api_endpoint}/counter"
+}
