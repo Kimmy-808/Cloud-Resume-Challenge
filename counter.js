12839cc27af0254d989f2ef8ce0006a81a897681
Kimmy-808 <kcruz18@valenciacollege.edu>
2026-01-05 03:39:57 -0500

    update counter api url

--- counter.js
+++ counter.js
@@ -1,6 +1,7 @@
-fetch("https://cd5kilshra.execute-api.us-east-2.amazonaws.com/prod/counter")
+fetch("https://1cych0h3g4.execute-api.us-east-2.amazonaws.com/counter")
   .then(response => response.json())
   .then(data => {
     document.getElementById("visitor-count").innerText = data.count;
   })
   .catch(error => console.error("Error fetching counter:", error));
+
